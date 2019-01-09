bfecb8616b8e27a9c6929ad0bee6c36b3c1290dd
Oluwatosin Awofeso <midastouchproductions@gmail.com>
2019-01-08 20:34:08 -0600

    finish the time app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import moment from "moment";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentTime: moment().format("LTS")
+    };
+  }
   showCurrentTime() {
-    console.log(moment());
+    let now = moment().format("LTS");
+    this.setState({ currentTime: now });
   }
   render() {
     return (
       <div className="App">
         <div onClick={() => this.showCurrentTime()} className="time-box">
-          <p> TIME </p>
+          <p>{this.state.currentTime}</p>
+        </div>
+        <div onClick={() => this.showCurrentTime()} className="time-box">
+          <p>{this.state.currentTime}</p>
+        </div>
+        <div onClick={() => this.showCurrentTime()} className="time-box">
+          <p>{this.state.currentTime}</p>
         </div>
       </div>
     );
